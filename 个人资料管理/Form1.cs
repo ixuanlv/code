f0c686ed745e18e76a2fdb8154690d22fa6ffc66
ixuanlv@vip.qq.com <xuanlv631>
2018-09-29 17:16:02 +0800

    第三天

--- 个人资料管理/Form1.cs
+++ 个人资料管理/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,38 +19,88 @@ namespace 个人资料管理
 
 
         }
+
+        public void conn()
+        {
+
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             treeView1.LabelEdit = true;
+
            
+            String fu = "select * from fu";          
+            DataSet Datafu = new DataSet();
+            DataSet Datazi = new DataSet();
+          
+            SqlConnection constr;
+            constr = new SqlConnection("server=.;database=ceshi;integrated security = SSPI");
+            constr.Open();
+            SqlDataAdapter sqlfu = new SqlDataAdapter(fu, constr);
+            sqlfu.Fill(Datafu, "1");
+            foreach (DataRow row in Datafu.Tables["1"].Rows)
+            {
+
+                String lablefu = row[2].ToString();
+                String name = row[1].ToString();
+                int sum = int.Parse(row[3].ToString());
+                treeView1.Nodes.Add(lablefu);
+                
+                    for (int i = 1; i < 2; i++)
+                    {
+                        String zi = "select * from zi where name='" + name + "'";
+                        SqlDataAdapter sqlzi = new SqlDataAdapter(zi, constr);
+                        sqlzi.Fill(Datazi, "1");
+                        foreach (DataRow row1 in Datazi.Tables["1"].Rows)
+                        {
+                            string lablezi = row1[2].ToString();
+                            string textzi = row1[3].ToString();
+                            Console.WriteLine(lablezi+"    "+textzi);
+                        }
+                    }
+                
+                          
+            }
+
+          
+
+            //String zi = "select * from zi where name='" + name + "'";
+            //SqlDataAdapter sqlzi = new SqlDataAdapter(zi, constr);
+            //sqlfu.Fill(Datazi, "1");
+
+            //foreach (DataRow row1 in Datazi.Tables["1"].Rows)
+            //{
+            //    String lablefu = row1[2].ToString();
+            //    String name = row1[1].ToString();
+            //    int sum = int.Parse1(row[3].ToString());
+
+            //}
+
+
+
+
         }
+     
+
+
+
+
 
         private void addgen_Click(object sender, EventArgs e)
         {
-            //text.Visible = false;
-            //Form2 gen = new Form2();
-            //gen.Show();
-            treeView1.Nodes.Add("新建文件");
-            //treeView1.LabelEdit = true;
-            //treeView1.SelectedNode.BeginEdit();
-
+            treeView1.Nodes.Add("默认标题");
         }
 
         private void addzi_Click(object sender, EventArgs e)
         {
-            //Form3 zi = new Form3();
-            //zi.Show();
-            //text.Visible = false;
             if (treeView1.SelectedNode != null)//判断是否存在选择节点
             {
-                treeView1.SelectedNode.Nodes.Add("新建文件");
+                treeView1.SelectedNode.Nodes.Add("默认标题");
             }
             else
             {
                 MessageBox.Show("请选择节点后操作");
-            }
-           
-           // treeView1.SelectedNode.BeginEdit();
+            }     
         }
 
 
